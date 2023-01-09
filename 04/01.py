68ca030adb4fdb672bbf2f346586a6497fd018e2
fumiyanll23 <fumiyanll23.public@gmail.com>
2023-01-09 22:27:58 +0900

    chore: a bit change

--- 04/01.py
+++ 04/01.py
@@ -1,25 +1,29 @@
-#必要なモジュールをインポート
+# 必要なモジュールをインポート
 import numpy as np
 
+
 def f(x0, x1, x2):
-    #目的関数の定義
-    y = x0**2 + x1**2 - x2*2 + 4*x0*x2 + 4*x1*x2 -3*x0 +2*x1 + x2 - 6
-    #勾配ベクトルの定義
-    dydxdz = np.array([2*x0 + 4*x2 - 3,2*x1 + 4*x2 +2,-2 + 4*x0 +4*x1+1])
-    #ヘッセ行列の定義
-    H = np.array([[2, 0, 4],[0, 2, 4],[4, 4, 0]])
+    # 目的関数の定義
+    y = x0**2 + x1**2 - x2*2 + 4*x0*x2 + 4*x1*x2 - 3*x0 + 2*x1 + x2 - 6
+    # 勾配ベクトルの定義
+    dydxdz = np.array([2*x0 + 4*x2 - 3, 2*x1 + 4*x2 + 2, -2 + 4*x0 + 4*x1 + 1])
+    # ヘッセ行列の定義
+    H = np.array([[2,0,4], [0,2,4], [4,4,0]])
+
     return y, dydxdz, H
-#初期座標を設定
+
+
+# 初期座標を設定
 x0, x1, x2 = 1, 1, 1
-#ステップ数、各種変数の推移を表示
+# ステップ数、各種変数の推移を表示
 print("i    x1          x2          x3          f(x)")
-#ニュートン法を実行
+# ニュートン法を実行
 for i in range(100):
     y, dydxdz, H = f(x0, x1, x2)
-    print(f"{i:3d} [{x0:10.3e}, {x1:10.3e},{x2:10.3e}], {y:10.3e}")
+    print(f"{i:3d} [{x0:10.3e}, {x1:10.3e}, {x2:10.3e}], {y:10.3e}")
     d = - np.dot(np.linalg.inv(H), dydxdz)
     x0 += d[0]
     x1 += d[1]
     x2 += d[2]
 
-#https://helve-blog.com/posts/math/newtons-method-python/　参照
+# 参考文献: ニュートン法による最適化とPythonによる実装(https://helve-blog.com/posts/math/newtons-method-python/)
